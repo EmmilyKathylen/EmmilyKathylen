6625f932efe9faf0ab21f0d935e2454801ccf98c
Emmily kathylen <emmilysouzakathylen@gmail.com>
2026-04-28 19:30:45 -0300

    exercicios

---
+++ teste.js
@@ -0,0 +1,37 @@
+/*
+let resultado = 2 + 6
+console.log(resultado)
+
+let a = true;
+let b = false;
+
+console.log (a && b);
+
+/////////////////////////////////////
+let nome = "emmily";
+let saudacao = "Oi," + nome;
+console.log(saudacao); 
+
+/////////////////////////////////////
+
+let idade = 16;
+
+if (idade >= 18) {
+    console.log("maior de idade");
+} else {
+    console.log("menor de idade");
+}
+
+/////////////////////////////////////
+
+for (let i = 0; i <10; i++){
+    console.log(i);
+}
+
+
+let x = 0;
+
+while (x < 5){
+    console.log(x);
+    x++;
+}*/
